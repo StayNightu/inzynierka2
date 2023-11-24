7b7d8c4d17df232b4c902eed8a2554e2d884620a
StayNightu <przemo828188@gmail.com>
2023-11-24 17:33:16 +0100

    end3

--- Sledzenie.py
+++ Sledzenie.py
@@ -1,3 +1,4 @@
+import csv
 from tkinter import filedialog
 import cv2
 import torch
@@ -61,6 +62,8 @@ class Sledzenie:
         return color
 
     def draw_boxes(self, img, bbox, identities=None, offset=(0, 0)):
+        overlay = np.zeros_like(img)
+
         for i, box in enumerate(bbox):
             x1, y1, x2, y2 = [int(i) for i in box]
             x1 += offset[0]
@@ -68,61 +71,84 @@ class Sledzenie:
             y1 += offset[0]
             y2 += offset[0]
             id = int(identities[i]) if identities is not None else 0
-            cv2.rectangle(img, (x1, y1), (x2, y2), color=self.compute_color_for_labels(id), thickness=2, lineType=cv2.LINE_AA)
+            cv2.rectangle(overlay, (x1, y1), (x2, y2), color=self.compute_color_for_labels(id), thickness=2,
+                          lineType=cv2.LINE_AA)
             label = str(id)
             t_size = cv2.getTextSize(str(label), cv2.FONT_HERSHEY_SIMPLEX, fontScale=1 / 2, thickness=1)[0]
             c2 = x1 + t_size[0], y1 - t_size[1] - 3
-            cv2.rectangle(img, (x1, y1), c2, color=self.compute_color_for_labels(id), thickness=-1, lineType=cv2.LINE_AA)
-            cv2.putText(img, str(label), (x1, y1 - 2), 0, 1 / 2, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
-        return img
+            cv2.rectangle(overlay, (x1, y1), c2, color=self.compute_color_for_labels(id), thickness=-1,
+                          lineType=cv2.LINE_AA)
+            cv2.putText(overlay, str(label), (x1, y1 - 2), 0, 1 / 2, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
+        combined_img = cv2.addWeighted(img, 1, overlay, 0.5, 0)
+        return combined_img
 
     def process_video(self):
-        self.out = cv2.VideoWriter('Output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (self.frame_width, self.frame_height))
-        prev_frame = None
-        while True:
-            xywh_bboxs = []
-            confs = []
-            oids = []
-            ret, frame = self.cap.read()
+        unique_id_counter = 0
+        with open('./bbox_data.csv', mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['detection_id', 'frame_number', 'x_top_left', 'y_top_left', 'width', 'height', 'bb_id'])
+            fourcc = cv2.VideoWriter_fourcc(*'XVID')
+            self.out = cv2.VideoWriter('output.avi', fourcc, 33.0, (1280, 720))
+            prev_frame = None
+            frame_number = 0
+            while True:
+                xywh_bboxs = []
+                confs = []
+                oids = []
+                outputs = []
+                ret, frame = self.cap.read()
 
-            if ret:
-                if prev_frame is not None:
-                    fg_mask = cv2.absdiff(prev_frame, frame)
-                    fg_mask = cv2.cvtColor(fg_mask, cv2.COLOR_BGR2GRAY)
-                    _, fg_mask = cv2.threshold(fg_mask, 5, 300, cv2.THRESH_BINARY)
-                    contours, _ = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                    if len(contours) > 0:
-                        result = list(self.model.predict(frame, conf=0.6))[0]
-                        bbox_xyxys = result.prediction.bboxes_xyxy.tolist()
-                        confidences = result.prediction.confidence
-                        labels = result.prediction.labels.tolist()
-                        for (bbox_xyxy, confidence, cls) in zip(bbox_xyxys, confidences, labels):
-                            bbox = np.array(bbox_xyxy)
-                            x1, y1, x2, y2 = bbox[0], bbox[1], bbox[2], bbox[3]
-                            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                            conf = math.ceil((confidence * 100)) / 100
-                            cx, cy = int((x1 + x2) / 2), int((y1 + y2) / 2)
-                            bbox_width = abs(x1 - x2)
-                            bbox_height = abs(y1 - y2)
-                            xcycwh = [cx, cy, bbox_width, bbox_height]
-                            if fg_mask[cy, cx] == 255:
-                                xywh_bboxs.append(xcycwh)
-                                confs.append(conf)
-                                oids.append(int(cls))
-                        xywhs = torch.tensor(xywh_bboxs)
-                        confss = torch.tensor(confs)
-                        outputs = self.deepsort.update(xywhs, confss, oids, frame)
-                        if len(outputs) > 0:
-                            bbox_xyxy = outputs[:, :4]
-                            identities = outputs[:, -2]
-                            object_id = outputs[:, -1]
-                            self.draw_boxes(frame, bbox_xyxy, identities, object_id)
-                prev_frame = frame.copy()
-                self.out.write(frame)
-            else:
-                break
-        self.out.release()
-        self.cap.release()
+                if ret:
+                    frame = cv2.resize(frame, (1280, 720))
+                    if prev_frame is not None:
+                        fg_mask = cv2.absdiff(prev_frame, frame)
+                        fg_mask = cv2.cvtColor(fg_mask, cv2.COLOR_BGR2GRAY)
+                        _, fg_mask = cv2.threshold(fg_mask, 5, 300, cv2.THRESH_BINARY)
+                        contours, _ = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                        if len(contours) > 0:
+                            result = list(self.model.predict(frame, conf=0.5))[0]
+                            bbox_xyxys = result.prediction.bboxes_xyxy.tolist()
+                            confidences = result.prediction.confidence
+                            labels = result.prediction.labels.tolist()
+                            for (bbox_xyxy, confidence, cls) in zip(bbox_xyxys, confidences, labels):
+                                bbox = np.array(bbox_xyxy)
+                                x1, y1, x2, y2 = bbox[0], bbox[1], bbox[2], bbox[3]
+                                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                                conf = math.ceil((confidence * 100)) / 100
+                                cx, cy = int((x1 + x2) / 2), int((y1 + y2) / 2)
+                                bbox_width = abs(x1 - x2)
+                                bbox_height = abs(y1 - y2)
+                                xcycwh = [cx, cy, bbox_width, bbox_height]
+                                if fg_mask[cy, cx] == 255:
+                                    xywh_bboxs.append(xcycwh)
+                                    confs.append(conf)
+                                    oids.append(int(cls))
+                            xywhs = torch.tensor(xywh_bboxs)
+                            confss = torch.tensor(confs)
+                            outputs = self.deepsort.update(xywhs, confss, oids, frame)
+                            if len(outputs) > 0:
+                                bbox_xyxy = outputs[:, :4]
+                                identities = outputs[:, -2]
+                                self.draw_boxes(frame, bbox_xyxy, identities)
+                    self.out.write(frame)
+                    for bbox_data in outputs:
+                        x1, y1, x2, y2, identity = bbox_data[:5]
+                        bbox_width = x2 - x1
+                        bbox_height = y2 - y1
+                        identity_formatted = f"id:{identity}"
+                        if identity < len(confs):
+                            writer.writerow(
+                                [unique_id_counter, frame_number, x1, y1, bbox_width, bbox_height, identity_formatted])
+                            unique_id_counter += 1
+                        else:
+                            print(f"Invalid identity index: {identity}")
+                    frame_number += 1
+                    prev_frame = frame.copy()
+                else:
+                    break
+
+            self.cap.release()
+            self.out.release()
 
     def run(self):
         self.set_video(self.video_source)
--- main.py
+++ main.py
@@ -1,33 +1,20 @@
 import tkinter as tk
-from Klatki import WyswietlanieKlatek
-from BBox import BoundingBox
 from Sledzenie import Sledzenie
-from dzielenie import dzielenie
-from TworzenieFilmu import Tworzenie
-from Wyswietl import Wyswietl
+from BBoxoverlay import BBoxOverlay
+from CSV_Edytor import CSVEditor
 class Main(tk.Tk):
-    def __init__(self, BBox_instance=None, Tworzenie_instance=None, Klatki_instance=None ):
+    def __init__(self):
         super().__init__()
         self.title("Edytor BBoxow")
-        self.canvas = tk.Canvas(self, width=640, height=480)
+        self.canvas = tk.Canvas(self, width=1280, height=720)
         self.canvas.pack(side=tk.TOP)
-        self.viewer = WyswietlanieKlatek(self.canvas)
-        if BBox_instance is None:
-            self.BBox = BoundingBox(main_instance=self, canvas=self.canvas)
-        else:
-            self.BBox = BBox_instance
-        if Tworzenie_instance is None:
-            self.TworzenieFilmu = Tworzenie()
-        else:
-            self.TworzenieFilmu = Tworzenie_instance
-        if Klatki_instance is None:
-            self.Klatki = WyswietlanieKlatek(self.canvas, self)
-        else:
-            self.Klatki = Klatki_instance
-        self.viewer = self.Klatki
+        video_path = 'output.avi'
+        csv_path = 'bbox_data.csv'
+        self.BBox = BBoxOverlay(video_path, csv_path, self)
         self.sledzenie = Sledzenie()
-        self.dzielenie = dzielenie(self)
+        self.canvas.bind("<Button-1>", self.on_canvas_click)
         self.initialize_ui()
+        self.csv_editor = None
 
     def initialize_ui(self):
         button_frame = tk.Frame(self)
@@ -36,34 +23,68 @@ class Main(tk.Tk):
         self.select_video_button = tk.Button(button_frame, text="Wybierz plik wideo", command=self.on_select_video)
         self.select_video_button.pack(side=tk.LEFT)
 
-        divide_video_button = tk.Button(button_frame, text="Podziel wideo na klatki", command=self.dzielenie.divide_video_into_frames)
-        divide_video_button.pack(side=tk.LEFT)
+        self.bbox_video_button = tk.Button(button_frame, text="Wideo z BBoxami", command=self.on_bbox_video)
+        self.bbox_video_button.pack(side=tk.LEFT)
+
+        self.next_frame_button = tk.Button(button_frame, text="Następna klatka", command=self.on_next_frame)
+        self.next_frame_button.pack(side=tk.LEFT)
+
+        self.prev_frame_button = tk.Button(button_frame, text="Poprzednia klatka", command=self.on_prev_frame)
+        self.prev_frame_button.pack(side=tk.LEFT)
+
+        self.select_bbox_button = tk.Button(button_frame, text="Zaznacz BBox", command=self.on_select_bbox_mode)
+        self.select_bbox_button.pack(side=tk.LEFT)
+
+        self.create_bbox_button = tk.Button(button_frame, text="Utwórz BBox", command=self.on_create_bbox)
+        self.create_bbox_button.pack(side=tk.LEFT)
+
+        self.edit_bbox_size_button = tk.Button(button_frame, text="Edytuj rozmiar BBox",
+                                               command=self.on_edit_bbox_size_mode)
+        self.edit_bbox_size_button.pack(side=tk.LEFT)
+
+        self.delete_bbox_button = tk.Button(button_frame, text="Usuń BBox", command=self.on_delete_bbox)
+        self.delete_bbox_button.pack(side=tk.LEFT)
+
+        self.open_csv_editor_button = tk.Button(button_frame, text="Otwórz edytor CSV", command=self.open_csv_editor)
+        self.open_csv_editor_button.pack(side=tk.LEFT)
 
 
-        display_button = tk.Button(button_frame, text="Wyświetl obraz", command=self.viewer.show_first_frame)
-        display_button.pack(side=tk.LEFT)
+        self.save_video_button = tk.Button(button_frame, text="Zapisz wideo z BBoxami", command=self.on_save_video)
+        self.save_video_button.pack(side=tk.LEFT)
 
-        next_button = tk.Button(button_frame, text="Następna klatka", command=self.viewer.next_frame)
-        next_button.pack(side=tk.LEFT)
+    def generate_new_id(self):
+        return max([bbox['id'] for bboxes in self.BBox.bbox_data.values() for bbox in bboxes], default=0) + 1
 
-        prev_button = tk.Button(button_frame, text="Poprzednia klatka", command=self.viewer.previous_frame)
-        prev_button.pack(side=tk.LEFT)
+    def generate_new_detection_id(self):
+        all_detection_ids = [bbox['detection_id'] for frame_bboxes in self.BBox.bbox_data.values() for bbox in frame_bboxes]
+        return max(all_detection_ids, default=0) + 1
 
-        self.draw_button = tk.Button(button_frame, text="Rysuj Bounding Box", command=self.BBox.toggle_drawing_mode)
-        self.draw_button.pack(side=tk.LEFT)
+    def on_prev_frame(self):
+        print("ebe")
+        frame = self.BBox.prev_frame()
+        if frame is not None:
+            self.update_canvas(frame)
 
-        del_button = tk.Button(button_frame, text="Usuń BBox", command=self.BBox.delete_selected_bbox)
-        del_button.pack(side=tk.LEFT)
+    def on_next_frame(self):
+        print("Następna klatka - metoda wywołana")  # Dodaj taki wydruk
+        frame = self.BBox.next_frame()
+        if frame is not None:
+            self.update_canvas(frame)
 
-        save_button = tk.Button(button_frame, text="Zapisz Bounding Boxy", command=self.viewer.save_boxes)
-        save_button.pack(side=tk.LEFT)
+    def on_canvas_click(self, event):
+        if self.BBox and self.BBox.select_bbox(event.x, event.y):
+            self.update_canvas(self.BBox.get_frame())
 
-        create_video_button = tk.Button(button_frame, text="Stwórz wideo", command=self.TworzenieFilmu.create_video)
-        create_video_button.pack(side=tk.LEFT)
+    def update_canvas(self, frame):
+        tk_frame = self.BBox.convert_frame_to_tkinter_format(frame)
+        self.canvas.create_image(0, 0, anchor=tk.NW, image=tk_frame)
+        self.canvas.image = tk_frame
 
-        wyswietl_button = tk.Button(button_frame, text="Wyświetl wideo", command=self.wyswietl_wideo)
-        wyswietl_button.pack(side=tk.LEFT)
-
+    def open_csv_editor(self):
+        csv_editor_window = tk.Toplevel(self)
+        csv_editor_window.title("Edytor CSV")
+        self.csv_editor = CSVEditor(csv_editor_window, './bbox_data.csv', self.refresh_frame)
+        csv_editor_window.protocol("WM_DELETE_WINDOW", self.on_csv_editor_close)
     def on_select_video(self):
         try:
             selected_video_path = self.sledzenie.load_video()
@@ -72,10 +93,120 @@ class Main(tk.Tk):
         except ValueError as e:
             print(e)
 
-    def wyswietl_wideo(self):
-        video_path = 'output2.mp4'
-        player = Wyswietl(self, video_path)
-        player.play_video()
+    def on_bbox_video(self):
+        video_path = './output.avi'
+        csv_path = './bbox_data.csv'
+        self.BBox = BBoxOverlay(video_path, csv_path, self)
+        self.BBox.paused = True
+        current_frame = self.BBox.get_frame()
+        if current_frame is not None:
+            self.update_canvas(current_frame)
+
+    def on_select_bbox_mode(self):
+        self.is_selecting_bbox = True
+        self.canvas.bind("<Button-1>", self.on_canvas_click_select_bbox)
+    def on_create_bbox(self):
+        self.canvas.bind("<ButtonPress-1>", self.on_button_press)
+        self.canvas.bind("<B1-Motion>", self.on_mouse_drag)
+        self.canvas.bind("<ButtonRelease-1>", self.on_button_release)
+        self.start_x = None
+        self.start_y = None
+        self.rect = None
+
+    def on_delete_bbox(self):
+        if self.BBox and self.BBox.selected_bbox:
+            self.BBox.delete_selected_bbox()
+            self.refresh_frame()
+            if self.csv_editor is not None:
+                self.csv_editor.refresh_data()
+
+    def on_button_press(self, event):
+        self.start_x = event.x
+        self.start_y = event.y
+        if self.rect:
+            self.canvas.delete(self.rect)
+        self.rect = self.canvas.create_rectangle(self.start_x, self.start_y, event.x, event.y, outline='red')
+
+    def on_mouse_drag(self, event):
+        curX, curY = (event.x, event.y)
+        if self.rect:
+            self.canvas.coords(self.rect, self.start_x, self.start_y, curX, curY)
+
+    def on_button_release(self, event):
+        end_x, end_y = (event.x, event.y)
+        self.save_bbox(self.start_x, self.start_y, end_x, end_y)
+        self.canvas.unbind("<ButtonPress-1>")
+        self.canvas.unbind("<B1-Motion>")
+        self.canvas.unbind("<ButtonRelease-1>")
+        if self.BBox:
+            current_frame = self.BBox.get_frame()
+            if current_frame is not None:
+                self.update_canvas(current_frame)
+
+    def on_edit_bbox_size_mode(self):
+        self.is_editing_bbox_size = True
+        self.canvas.bind("<ButtonPress-1>", self.on_bbox_resize_press)
+        self.canvas.bind("<B1-Motion>", self.on_bbox_resize_drag)
+        self.canvas.bind("<ButtonRelease-1>", self.on_bbox_resize_release)
+
+    def on_bbox_resize_press(self, event):
+        if self.BBox and self.is_editing_bbox_size:
+            self.BBox.start_resizing_bbox(event.x, event.y)
+
+    def on_bbox_resize_drag(self, event):
+        if self.BBox and self.is_editing_bbox_size:
+            self.BBox.resize_bbox(event.x, event.y)
+            self.update_canvas(self.BBox.get_frame())
+
+    def on_bbox_resize_release(self, event):
+        if self.BBox and self.is_editing_bbox_size:
+            self.BBox.finish_resizing_bbox()
+            self.BBox.save_bbox_data()
+            if self.csv_editor is not None:
+                self.csv_editor.refresh_data()
+            self.update_canvas(self.BBox.get_frame())
+
+
+    def save_bbox(self, x1, y1, x2, y2):
+        bbox_width = abs(x2 - x1)
+        bbox_height = abs(y2 - y1)
+        new_bbox = {
+            'detection_id': self.generate_new_detection_id(),
+            'x1': min(x1, x2),
+            'y1': min(y1, y2),
+            'width': bbox_width,
+            'height': bbox_height,
+            'id': self.generate_new_id(),
+        }
+        self.BBox.save_new_bbox(new_bbox)
+        self.refresh_frame()
+        self.BBox.save_bbox_data()
+        if self.csv_editor is not None:
+            self.csv_editor.refresh_data()
+
+    def refresh_frame(self):
+        if self.BBox:
+            self.BBox.update_bbox_data()
+            current_frame = self.BBox.get_frame()
+            if current_frame is not None:
+                self.update_canvas(current_frame)
+
+    def on_csv_editor_close(self):
+        if self.csv_editor is not None:
+            self.csv_editor.master.destroy()
+            self.csv_editor = None
+
+    def on_canvas_click_select_bbox(self, event):
+        if self.is_selecting_bbox and self.BBox:
+            if self.BBox.select_bbox(event.x, event.y):
+                self.update_canvas(self.BBox.get_frame())
+            else:
+                self.BBox.selected_bbox = None
+                self.update_canvas(self.BBox.get_frame())
+
+    def on_save_video(self):
+        output_path = 'output_z_bboxami.avi'
+        self.BBox.save_video_with_bbox(output_path)
 
 if __name__ == "__main__":
     app = Main()
